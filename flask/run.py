86c2f90c9f561d33d49ec609368f90a5c3844f14
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-05-11 18:57:56 +0000

    updated run.py

--- flask/run.py
+++ flask/run.py
@@ -3,20 +3,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html") 
+    return render_template("about.html")
 
-@app.route("/contact")
-def about():
-    return render_template("contact.html")       
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
-if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
