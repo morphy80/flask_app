10875a9092cdfaa4906e39b6e9d21f4f28b8c8c7
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-05-12 12:38:38 +0000

    added json

--- flask/run.py
+++ flask/run.py
@@ -13,7 +13,7 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", company=data)
+    return render_template("about.html", page_title="About", company_data=data)
 
 @app.route("/contact")
 def contact():
